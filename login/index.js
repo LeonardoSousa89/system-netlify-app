749cd11faedc5003b8880895921944cf53c481aa
Leozin89python <leozin069@gmail.com>
2021-09-01 00:47:01 -0300

    app

--- login/index.js
+++ login/index.js
@@ -1,3 +1,46 @@
 const doc  = document
 doc.onclick = addEventListener('click',(e)=>e.preventDefault())
- 
+doc.onload  = addEventListener('load',()=>{
+   $('#alert_login').hide()
+   $('#alert_empty').hide()
+}) 
+
+$('#enter').click(function(){
+    let email = document.querySelector('#floatingInput'   ).value
+    let pass  = document.querySelector('#floatingPassword').value
+
+    if(email == '' || pass == ''){
+        $('#alert_empty').show(100)
+    }else{
+        const url = '../login/mock.json'
+
+        fetch(url)
+            .then(data => data.json())
+            .then(data => {
+                data.filter(e=>{
+                    if(e.user == email && e.password == pass){
+                        doc.location.href = '../store/index.html'
+                        storage(email, pass)
+                    }else{
+                        $('#alert_login').show(100)
+                    }
+                })
+            })
+            .catch(err => console.log(err))
+    }
+})
+
+$('#floatingInput').mouseenter(function(){
+    $('#alert_login').hide(100)
+    $('#alert_empty').hide(100)
+})
+
+$('#floatingPassword').mouseenter(function(){
+    $('#alert_login').hide(100)
+    $('#alert_empty').hide(100)
+})
+
+function storage(email, pass){
+    localStorage.setItem('_USER',email)
+    localStorage.setItem('_PASS',pass)
+}
